ee73b08f4e8d61314b484b998f18774a15964b00
qiaofeng1227 <qiaofeng1227@users.noreply.github.com>
2022-07-14 06:41:04 +0000

    new vendor

--- apps/add
+++ apps/add
@@ -1,1 +1,1 @@
-wordpress
+
---
+++ apps/roles/wordpress/templates/rabbitmq_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export ORACLE_BASE=/opt/oracle 
+export ORACLE_HOME=/opt/oracle/product/18c/dbhomeXE
+export ORACLE_SID=XE
+export PATH=$PATH:$ORACLE_HOME/bin
