d3b19c9522b38fd9f6bdc671934944e45c33fec6
qiaofeng1227 <76487013@qq.com>
2022-07-15 16:02:54 +0800

    Update containers_version.sh

--- scripts/containers_version.sh
+++ scripts/containers_version.sh
@@ -2,7 +2,11 @@
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 
 # get applist
-git clone --depth=1 https://github.com/Websoft9/StackHub.git
+if [ -d StackHub ] ; then
+  echo "StackHub is exists"
+else
+  git clone --depth=1 https://github.com/Websoft9/StackHub.git
+fi 
 appnames=$(ls StackHub/apps/roles |grep -v Template |grep -v README.md |grep -v role)
 for appname in $appnames; do                                   
     echo $character
