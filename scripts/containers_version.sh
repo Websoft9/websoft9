0d5030a4fe5e46e9e1fb7f2e2b2c355f28dc5630
qiaofeng1227 <76487013@qq.com>
2022-07-15 15:57:02 +0800

    Update containers_version.sh

--- scripts/containers_version.sh
+++ scripts/containers_version.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
+
+# get applist
+git clone --depth=1 https://github.com/Websoft9/StackHub.git
+appnames=$(ls StackHub/apps/roles |grep -v Template |grep -v README.md |grep -v role)
+for appname in $appnames; do                                   
+    echo $character
+done 
