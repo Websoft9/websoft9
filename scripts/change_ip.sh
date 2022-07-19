52eacfabcbd4423658bdca096f79515ef6ad8f6a
qiaofeng1227 <76487013@qq.com>
2022-07-19 16:40:30 +0800

    Update change_ip.sh

--- scripts/change_ip.sh
+++ scripts/change_ip.sh
@@ -3,7 +3,7 @@ app_url_replace=$(cat /data/apps/$1/.env |grep APP_URL_REPLACE)
 if [ $app_url_replace == "APP_URL_REPLACE=true" ]; then
   echo "Change APP_URL"
   public_ip=`wget -O - https://download.websoft9.com/ansible/get_ip.sh | bash`
-  sudo sed -i "s/APP_URL=.*/APP_URL=$public_ip/g" /data/apps/{{app_name}}/.env
+  sudo sed -i "s/APP_URL=.*/APP_URL=$public_ip/g" /data/apps/$1/.env
 else
   echo "There is not APP_URL"
 fi 
