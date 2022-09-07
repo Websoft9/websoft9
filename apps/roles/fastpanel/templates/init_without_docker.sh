11cf710516c3d761927813e824c2be649e823ea2
qiaofeng1227 <76487013@qq.com>
2022-09-07 16:42:18 +0800

    Update init_without_docker.sh

--- apps/roles/fastpanel/templates/init_without_docker.sh
+++ apps/roles/fastpanel/templates/init_without_docker.sh
@@ -9,4 +9,3 @@ internal_ip=$(ip addr show dev eth0|grep inet |grep eth0|cut -d/ -f1|cut -d" " -
 old_ip=$(cat /etc/nginx/conf.d/parking.conf |grep default_server|grep ssl |cut -d: -f1|cut -d" " -f6)
 sed -i "s/$old_ip/$internal_ip/g" /etc/nginx/conf.d/parking.conf
 systemctl restart nginx
-
