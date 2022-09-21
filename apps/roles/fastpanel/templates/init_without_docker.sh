79a1024e29ef49a36680f1b090a4cf0b30e5059e
qiaofeng1227 <76487013@qq.com>
2022-09-21 11:52:13 +0800

    Update init_without_docker.sh

--- apps/roles/fastpanel/templates/init_without_docker.sh
+++ apps/roles/fastpanel/templates/init_without_docker.sh
@@ -2,7 +2,8 @@
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 
 newpassword=$1
-echo $newpassword |passwd --stdin fastuser
+# echo $newpassword |passwd --stdin fastuser
+echo "fastuser:$newpassword" | chpasswd
 
 sed -i '/172.17.0.1/d' /etc/nginx/conf.d/parking.conf
 internal_ip=$(ip addr show dev eth0|grep inet |grep eth0|cut -d/ -f1|cut -d" " -f6)
