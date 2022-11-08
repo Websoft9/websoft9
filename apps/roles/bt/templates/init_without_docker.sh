4dca9b043db625d9d211ba2173e4c65729e300c5
qiaofeng1227 <76487013@qq.com>
2022-11-08 10:39:41 +0800

    Update init_without_docker.sh

--- apps/roles/bt/templates/init_without_docker.sh
+++ apps/roles/bt/templates/init_without_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 new_password=$1
+echo "init_without_docker.sh password=$new_password" >> /tmp/init_debug.txt
 sudo su && cd /www/server/panel && python tools.py panel $new_password
