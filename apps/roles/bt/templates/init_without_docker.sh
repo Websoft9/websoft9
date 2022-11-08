9f6fd8992064f1ae2bdacf351332dca1ed90aacd
chendelin1982 <27513732@qq.com>
2022-11-08 11:26:54 +0800

    bt

--- apps/roles/bt/templates/init_without_docker.sh
+++ apps/roles/bt/templates/init_without_docker.sh
@@ -2,4 +2,5 @@
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 new_password=$1
 echo "init_without_docker.sh password=$new_password" >> /tmp/init_debug.txt
-sudo su && cd /www/server/panel && python tools.py panel $new_password
+sudo sed -i "s/admin123/$new_password/g" /data/apps/bt/password.exp
+expect /data/apps/bt/password.exp
