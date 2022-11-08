a96414c4bfd4c703174a9ec60d1b8fec4ee72183
qiaofeng1227 <76487013@qq.com>
2022-11-08 10:38:43 +0800

    Update init_without_docker.sh

--- apps/roles/bt/templates/init_without_docker.sh
+++ apps/roles/bt/templates/init_without_docker.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
-
 new_password=$1
-sudo su
-cd /www/server/panel
-python tools.py panel $new_password
+sudo su && cd /www/server/panel && python tools.py panel $new_password
