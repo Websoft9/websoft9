100438f9ee5c06b69f1494f8f5fdfaeb302af826
qiaofeng1227 <76487013@qq.com>
2022-11-23 17:38:39 +0800

    Update init_without_docker.sh

--- apps/roles/mingdao/templates/init_without_docker.sh
+++ apps/roles/mingdao/templates/init_without_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
-cd  /data/apps/mingdao/installer && bash ./service.sh restart    
+cd  /data/apps/mingdao/installer && bash ./service.sh start    
