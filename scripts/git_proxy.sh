5ce4a05c4166121b3788e0aa1c1336494863831e
徐月婷 <93903193+xyt52@users.noreply.github.com>
2022-12-30 10:40:17 +0800

    Update git_proxy.sh

--- scripts/git_proxy.sh
+++ scripts/git_proxy.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+#repo=$1
+#path=$2
 
+git_clone(){
+    for r in ${1[*]}
+    do
+	filename=$(echo $r | cut -d '/' -f 2)
+        for x in ${2[*]}
+        do      
+	     if [ -e $filename ]; then
+		  echo "$filename was cloned successfully"                   
+		  break
+	     else
+		  timeout -k 1 3 git clone $x/$r.git
+	     fi 	
+        done
+    done
+}
+
+git_clone
