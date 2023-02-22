ffc4db75fc209cbd5beb5f075f1d9a987b8834af
qiaofeng1227 <76487013@qq.com>
2023-02-22 09:35:21 +0800

    Update docker.py

--- appmanage/api/utils/docker.py
+++ appmanage/api/utils/docker.py
@@ -1,1 +1,11 @@
+import os, io, sys, platform, shutil, time, json, datetime
 
+def create_app_directory(app_name):
+  
+    #1.判断/data/apps/app_name是否已经存在，如果已经存在，方法结束
+
+    #2. git clone https://github.com/Websoft9/docker-library.git项目，将apps复制到/data目录，如果data目录没有，创建
+
+def check_app_compose(app_name):
+  
+    #1.判断/data/apps/app_name/.env中的port是否占用，没有被占用，方法结束（network.py的get_start_port方法）
