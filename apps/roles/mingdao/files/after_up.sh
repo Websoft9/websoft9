62f44f72a25c4cdd6880aff72e63d3f113e8940a
Darren <27513732@qq.com>
2023-03-01 16:23:20 +0800

    Update after_up.sh

--- apps/roles/mingdao/files/after_up.sh
+++ apps/roles/mingdao/files/after_up.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf /data/mingdao
 /bin/rm -f /usr/local/MDPrivateDeployment/first  
 /bin/bash /usr/local/MDPrivateDeployment/service.sh stopall  
 /bin/bash /usr/local/MDPrivateDeployment/service.sh start
