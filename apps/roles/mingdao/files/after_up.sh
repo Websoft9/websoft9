d0ebf3d7831f5715f7eca3cd5f73b18f02916e43
qiaofeng1227 <76487013@qq.com>
2023-03-02 14:17:53 +0800

    Update after_up.sh

--- apps/roles/mingdao/files/after_up.sh
+++ apps/roles/mingdao/files/after_up.sh
@@ -4,5 +4,5 @@ sudo systemctl stop mingdao
 sudo docker stop $(docker ps -aq)
 sudo docker rm $(docker ps -aq)
 rm -rf /data/mingdao
-/bin/rm -f /data/apps/mingdao/installer/first
+rm -f /data/apps/mingdao/installer/first
 sudo systemctl start mingdao
