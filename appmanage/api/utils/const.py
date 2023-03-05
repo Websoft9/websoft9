d7ba4098e7019c11cb028cc2640b0032ab452f98
qiaofeng1227 <76487013@qq.com>
2023-03-06 07:11:27 +0800

    Create const.py

---
+++ appmanage/api/utils/const.py
@@ -0,0 +1,20 @@
+# 所有常量统一定义区
+
+# 接口返回值定义
+# 成功
+RETURN_SUCCESS = 0
+# 失败
+RETURN_FAIL = -1
+
+# 应用状态定义
+# 应用正在启动
+RETURN_RUNNING = 0
+# 应用正在运行中
+RETURN_RUNNING = 1
+# 应用已经停止
+RETURN_STOP = 2
+# 应用错误或不存在
+RETURN_STOP = -1
+
+
+
