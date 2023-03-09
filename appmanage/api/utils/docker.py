aa79c83cdfed68036d0a7fee3de18b9a9a785d2b
qiaofeng1227 <76487013@qq.com>
2023-03-09 12:55:09 +0800

    Update docker.py

--- appmanage/api/utils/docker.py
+++ appmanage/api/utils/docker.py
@@ -14,6 +14,7 @@ def get_process_perc(app_name):
     app_version = read_env(path, "APP_VERSION")
     client = docker.from_env()
     image_name = app_name + ":" + app_version
+    print(resp)
     resp = client.api.pull(image_name, stream=True, decode=True)
     print(resp)
     #for line in resp:
