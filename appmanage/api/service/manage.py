db20c7a86042933eb6f1dff1f3ffb6785c789aa0
qiaofeng1227 <76487013@qq.com>
2023-03-14 10:14:47 +0800

    Update manage.py

--- appmanage/api/service/manage.py
+++ appmanage/api/service/manage.py
@@ -170,7 +170,7 @@ def install_app(app_name, customer_app_name, app_version):
     unique_app_path = "/data/apps/"+customer_app_name
     
     # 防止app名重复
-    if os.path.exists(unique_app_path)
+    if os.path.exists(runnging_file_path)
         ret = Response(code=const.RETURN_FAIL , message="APP名称已经使用，请指定其他名称重新安装。")
         ret = ret.dict()
         return ret
