d663f4480c5622abc35b8533935d66e3dc0b4f3d
qiaofeng1227 <76487013@qq.com>
2023-03-14 09:33:06 +0000

    middleware

--- appmanage/main.py
+++ appmanage/main.py
@@ -3,6 +3,7 @@ import logging
 import api.v1.api as api_router_v1
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 logging_format = '[%(asctime)s][%(levelname)s] %(message)s'
 logging.basicConfig(format=logging_format, level=logging.DEBUG)
@@ -12,6 +13,19 @@ logging.info("Starting server")
 
 def get_app():
     app = FastAPI()
+    origins = [
+        "http://localhost",
+        "http://localhost:3000",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     app.include_router(api_router_v1.get_api(), prefix="/api/v1")
     return app
 
