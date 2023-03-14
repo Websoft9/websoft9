234feaf27cfa9f45db713faf816178906dd78346
qiaofeng1227 <76487013@qq.com>
2023-03-14 09:36:18 +0000

    force install

--- appmanage/api/v1/routers/apps.py
+++ appmanage/api/v1/routers/apps.py
@@ -18,7 +18,7 @@ def list_my_apps():
 
 @router.get("/install")
 def install_app(app_name: Optional[str] = None, customer_app_name: Optional[str] = None, app_version: Optional[str] = None, app_force: Optional[bool] = False):
-    ret = manage.install_app(app_name, customer_app_name, app_version)
+    ret = manage.install_app(app_name, customer_app_name, app_version, app_force)
     return JSONResponse(content=ret)
 
 @router.get("/process")
