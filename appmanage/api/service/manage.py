13cef3893f169ca952781928f4d8cbf2e497dcf0
qiaofeng1227 <76487013@qq.com>
2023-03-15 00:59:06 +0000

    format

--- appmanage/api/service/manage.py
+++ appmanage/api/service/manage.py
@@ -1,4 +1,12 @@
-import os, io, sys, platform, shutil, time, subprocess, json, datetime
+import os
+import io
+import sys
+import platform
+import shutil
+import time
+import subprocess
+import json
+import datetime
 import socket
 from threading import Thread
 from api.utils import shell_execute, network, docker, const
@@ -7,7 +15,7 @@ from api.model.response import Response
 
 # 获取所有app的信息
 def get_my_app(app_name=None):
-    #{"name":"id",...}
+
     ret = Response(code=const.RETURN_FAIL, message="app查询失败")
 
     # get all info
@@ -27,9 +35,11 @@ def get_my_app(app_name=None):
                     flag = 1
                     break
         if app_name == None or flag == 1:
-            ret = Response(code=const.RETURN_SUCCESS, message="app查询成功", data=list)
+            ret = Response(code=const.RETURN_SUCCESS,
+                           message="app查询成功", data=list)
     ret = ret.dict()
     return ret
+
 
 def set_app_info(output_list):
     ip_result = shell_execute.execute_command_output_all("curl ifconfig.me")
@@ -39,7 +49,8 @@ def set_app_info(output_list):
         app_name = app_info.split()[0]  # app_name
 
         true_name = docker.read_var(app_name, 'name')
-        image_url = "https://libs.websoft9.com/Websoft9/logo/product/" + true_name + "-websoft9.png"
+        image_url = "https://libs.websoft9.com/Websoft9/logo/product/" + \
+            true_name + "-websoft9.png"
         # get trade_mark
         trade_mark = docker.read_var(app_name, 'trademark')
         id = app_name  # id
@@ -61,14 +72,16 @@ def set_app_info(output_list):
         admin_url = "-"
         # get port and url
         try:
-            http_port = list(docker.read_env(path, "APP_HTTP_PORT").values())[0]
+            http_port = list(docker.read_env(
+                path, "APP_HTTP_PORT").values())[0]
             port = int(http_port)
             easy_url = "http://" + ip + ":" + str(port)
             url = get_url(app_name, easy_url)
             admin_url = get_admin_url(app_name, url)
         except IndexError:
             try:
-                db_port = list(docker.read_env(path, "APP_DB.*_PORT").values())[0]
+                db_port = list(docker.read_env(
+                    path, "APP_DB.*_PORT").values())[0]
                 port = int(db_port)
             except IndexError:
                 pass
@@ -81,12 +94,13 @@ def set_app_info(output_list):
         # get password
         password = "-"
         try:
-            password = list(docker.read_env(path, "POWER_PASSWORD").values())[0]
+            password = list(docker.read_env(
+                path, "POWER_PASSWORD").values())[0]
         except IndexError:
             pass
 
         true_name = docker.read_var(app_name, 'name')
-        app = App(id=id, name=true_name, customer_name = app_name ,status_code=case_code, status=case, port=port, volume=volume, url=url,
+        app = App(id=id, name=true_name, customer_name=app_name, status_code=case_code, status=case, port=port, volume=volume, url=url,
                   image_url=image_url, admin_url=admin_url, trade_mark=trade_mark, user_name=user_name, password=password)
         app_list.append(app.dict())
 
@@ -94,16 +108,18 @@ def set_app_info(output_list):
     if os.path.exists(file_path) and os.path.getsize(file_path):
         with open(file_path, "r", encoding="utf-8") as f:
             for running_app_name in f:
-                image_url = "https://libs.websoft9.com/Websoft9/logo/product/" + running_app_name + "-websoft9.png"
+                image_url = "https://libs.websoft9.com/Websoft9/logo/product/" + \
+                    running_app_name + "-websoft9.png"
                 trade_mark = docker.read_var(app_name, 'trademark')
                 true_name = docker.read_var(app_name, 'name')
-                app = App(id=running_app_name, name=true_name, customer_name = running_app_name, status_code=const.RETURN_READY, status="installing", port=0, volume="-",
-                          url="-",image_url=image_url, admin_url="-", trade_mark=trade_mark, user_name="-",password="-")
+                app = App(id=running_app_name, name=true_name, customer_name=running_app_name, status_code=const.RETURN_READY, status="installing", port=0, volume="-",
+                          url="-", image_url=image_url, admin_url="-", trade_mark=trade_mark, user_name="-", password="-")
                 app_list.append(app.dict())
     return app_list
 
-def get_url(app_name,easy_url):
-    
+
+def get_url(app_name, easy_url):
+
     url = easy_url
     if app_name == "joomla":
         url = easy_url + "/administrator"
@@ -113,8 +129,9 @@ def get_url(app_name,easy_url):
         url = easy_url
     return url
 
-def get_admin_url(app_name,url):
-    
+
+def get_admin_url(app_name, url):
+
     admin_url = "-"
     if app_name == "wordpress":
         admin_url = url + "/wp-admin"
@@ -124,6 +141,7 @@ def get_admin_url(app_name,url):
         admin_url = "-"
     return admin_url
 
+
 def install_app_process(app_name):
 
     if docker.check_app_directory(app_name):
@@ -131,13 +149,14 @@ def install_app_process(app_name):
         ret = Response(code=const.RETURN_SUCCESS, message=percentage)
         ret = ret.dict()
     else:
-        ret = Response(code=const.RETURN_FAIL , message="目前没有安装此App")
+        ret = Response(code=const.RETURN_FAIL, message="目前没有安装此App")
         ret = ret.dict()
     return ret
 
-def install_app(app_name, customer_app_name , app_version, app_force):
 
-    #if app_force == False:
+def install_app(app_name, customer_app_name, app_version, app_force):
+
+    # if app_force == False:
     #    if docker.check_vm_resource(app_name) == False:
     #        ret = Response(code=const.RETURN_FAIL , message="系统资源不足，继续安装可能导致应用无法运行或服务器异常！")
     #        ret = ret.dict()
@@ -153,14 +172,16 @@ def install_app(app_name, customer_app_name , app_version, app_force):
 
     # 防止app名重复
     if if_app_exits(customer_app_name):
-        ret = Response(code=const.RETURN_FAIL , message="APP名称已经使用，请指定其他名称重新安装。")
+        ret = Response(code=const.RETURN_FAIL,
+                       message="APP名称已经使用，请指定其他名称重新安装。")
         ret = ret.dict()
         return ret
 
     elif docker.check_app_directory(app_name):
-        
+
         if app_name != customer_app_name:
-            output = shell_execute.execute_command_output_all("cp -r /data/apps/" + app_name + " /data/apps/" + customer_app_name)
+            output = shell_execute.execute_command_output_all(
+                "cp -r /data/apps/" + app_name + " /data/apps/" + customer_app_name)
             if int(output["code"]) != 0:
                 ret.code = const.RETURN_FAIL
                 ret.message = "创建" + customer_app_name + "目录失败."
@@ -168,7 +189,7 @@ def install_app(app_name, customer_app_name , app_version, app_force):
                 return ret
             var_file = unique_app_path + '/variables.json'
             docker.modify_env(var_file, 'APP_NAME', customer_app_name)
-  
+
         # check port
         docker.check_app_compose(customer_app_name)
         if app_version != None:
@@ -179,9 +200,10 @@ def install_app(app_name, customer_app_name , app_version, app_force):
         ret = Response(code=const.RETURN_SUCCESS, message="应用正在启动中，请过几分钟再查询")
         ret = ret.dict()
     else:
-        ret = Response(code=const.RETURN_FAIL , message="目前不支持安装此App")
+        ret = Response(code=const.RETURN_FAIL, message="目前不支持安装此App")
         ret = ret.dict()
     return ret
+
 
 def record_and_install_app(app_name):
     # modify running_apps.txt
@@ -193,6 +215,7 @@ def record_and_install_app(app_name):
     with open(file_path, "a+", encoding="utf-8") as f:
         f.truncate(0)
 
+
 def if_app_exits(app_name):
     cmd = "docker compose ls -a | grep \'"+app_name+"\\b\'"
     output = shell_execute.execute_command_output_all(cmd)
@@ -200,6 +223,7 @@ def if_app_exits(app_name):
         return False
     else:
         return True
+
 
 def start_app(app_name):
     ret = Response(code=const.RETURN_FAIL, message="")
@@ -217,6 +241,7 @@ def start_app(app_name):
     ret = ret.dict()
     return ret
 
+
 def stop_app(app_name):
     ret = Response(code=const.RETURN_FAIL, message="")
     if if_app_exits(app_name):
@@ -232,6 +257,7 @@ def stop_app(app_name):
     ret = ret.dict()
     return ret
 
+
 def restart_app(app_name):
     ret = Response(code=const.RETURN_FAIL, message="")
     if if_app_exits(app_name):
@@ -246,6 +272,7 @@ def restart_app(app_name):
         ret.message = "应用不存在"
     ret = ret.dict()
     return ret
+
 
 def uninstall_app(app_name, delete_flag):
     ret = Response(code=const.RETURN_FAIL, message="")
