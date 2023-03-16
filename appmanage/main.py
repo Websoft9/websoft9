a5aabf3744f7ef23dea5cfa6317286ec5b7cc430
qiaofeng1227 <76487013@qq.com>
2023-03-16 08:39:55 +0800

    Update main.py

--- appmanage/main.py
+++ appmanage/main.py
@@ -12,6 +12,8 @@ def get_app():
     origins = [
         "http://localhost",
         "http://localhost:3000",
+        "http://localhost:3001",
+        "http://localhost:3002",
     ]
 
     app.add_middleware(
