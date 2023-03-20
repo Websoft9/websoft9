7250443fae64b42a900e92e0722a8e9755d9414b
qiaofeng1227 <76487013@qq.com>
2023-03-20 12:46:54 +0800

    Update main.py

--- appmanage/main.py
+++ appmanage/main.py
@@ -20,7 +20,7 @@ def get_app():
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
-        allow_credentials=True,
+        allow_credentials=False,
         allow_methods=["*"],
         allow_headers=["*"],
     )
