a0c2da36854b675369e5ea2e717f980c7973951b
TooY <330282372@qq.com>
2023-03-24 09:43:49 +0800

    update manage.py docker.py

--- appmanage/api/service/manage.py
+++ appmanage/api/service/manage.py
@@ -205,7 +205,7 @@ def check_app(app_name, customer_app_name, app_version):
 
 
 def prepare_app(app_name, customer_app_name):
-    library_path = "/data/library/" + app_name
+    library_path = "/data/apps/docker-library/" + app_name
     install_path = "/data/apps/" + customer_app_name
     message = " "
     code = const.RETURN_SUCCESS
