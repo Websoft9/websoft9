17ae6182ef110d52f0925169af3a958e8ac5c855
qiaofeng1227 <76487013@qq.com>
2023-03-24 14:02:24 +0800

    Update shell_execute.py

--- appmanage/api/utils/shell_execute.py
+++ appmanage/api/utils/shell_execute.py
@@ -14,7 +14,7 @@ def execute_command_output_all(cmd_str, max_time = 3):
     myLogger.info_logger("Start to execute cmd: " + cmd_str)
     execute_time = 0
     while execute_time < max_time:
-        process = subprocess.run(f'nsenter -m -u -i -n -p -t 1 sh -c "{cmd_str}"', capture_output=True, check=True, text=True, shell=True)
+        process = subprocess.run(convert_command(cmd_str), capture_output=True, check=True, text=True, shell=True)
         if process.returncode == 0:
             return {"code": "0", "result": process.stdout,}
         else:
@@ -28,6 +28,6 @@ def convert_command(cmd_str):
     if cmd_str == "":
        convert_cmd=cmd_str
     else:
-       convert_cmd="f nsenter -m -u -i -n -p -t 1 sh -c " +cmd_str
+       convert_cmd="nsenter -m -u -i -n -p -t 1 sh -c " + "'"+cmd_str+"'"
 
     return convert_cmd
