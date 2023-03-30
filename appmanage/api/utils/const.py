d75978d5f127e5877709385fb2a061e292706f10
qiaofeng1227 <76487013@qq.com>
2023-03-30 15:07:46 +0800

    Update const.py

--- appmanage/api/utils/const.py
+++ appmanage/api/utils/const.py
@@ -7,16 +7,22 @@ RETURN_SUCCESS = 0
 RETURN_FAIL = -1
 
 # 应用状态定义
-# 应用启动准备 installing
-RETURN_READY = 0
-# 应用正在运行
-RETURN_RUNNING = 1
-# 应用已经停止
-RETURN_STOP = 2
+# 应用启动中 installing
+APP_READY = 0
+# 应用正在运行 running
+APP_RUNNING = 1
+# 应用已经停止 stop
+APP_STOP = 2
 # 应用等待安装 waiting
-RETURN_WAIT = 3
-# 应用错误或不存在
-RETURN_ERROR = -1
+APP_WAIT = 3
+# 应用错误或不存在 容器一直restart
+APP_ERROR = -1
+
+# 应用正在运行状态定义 (pulling,creating,initing,running)
+APP_RUNNING_STATE_PULLING = 10
+APP_RUNNING_STATE_CREATing = 11
+APP_RUNNING_STATE_INITING = 12
+APP_RUNNING_STATE_RUNNING = 13
 
 
 
