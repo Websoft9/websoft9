370e7ccab999b10fc5fb8af473f28094cc7245ea
qiaofeng1227 <76487013@qq.com>
2023-04-14 03:20:44 +0000

    status

--- appmanage/api/utils/const.py
+++ appmanage/api/utils/const.py
@@ -1,25 +1,13 @@
 # 所有常量统一定义区
 
-# 接口返回值定义
-# 成功
-RETURN_SUCCESS = 0
-# 失败
-RETURN_FAIL = -1
-
 # 应用状态定义
 # 应用启动中 installing
-APP_READY = 0
+APP_STATUS_INSTALLING = "installing"
 # 应用正在运行 running
-APP_RUNNING = 1
-# 应用已经停止 stop
-APP_STOP = 2
-# 应用等待安装 waiting
-APP_WAIT = 3
-# 应用错误或不存在 容器一直restart
-APP_ERROR = -1
-
-# 应用正在运行状态定义 (pulling,creating,initing,running)
-APP_RUNNING_STATE_PULLING = 10
-APP_RUNNING_STATE_CREATING = 11
-APP_RUNNING_STATE_INITING = 12
-APP_RUNNING_STATE_RUNNING = 13
+APP_STATUS_RUNNING = "running"
+# 应用已经停止 exited
+APP_STATUS_EXITED = "exited"
+# 应用不断重启 restarting
+APP_STATUS_RESTARTING = "restarting"
+# 应用错误 failed
+APP_STATUS_FAILED = "failed"
