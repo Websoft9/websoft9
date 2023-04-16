bea4fe9b3c6b43afe9841ce66cca6abda1b3fa1d
qiaofeng1227 <76487013@qq.com>
2023-04-16 23:08:19 +0800

    Update apps.py

--- appmanage/api/v1/routers/apps.py
+++ appmanage/api/v1/routers/apps.py
@@ -70,6 +70,7 @@ def AppList(request: Request, app_id: Optional[str] = Query(default=None, descri
         myLogger.info_logger("Receive request: /AppList")
         get_headers(request)
         ret = {}
+        ret['ResponseData'] = {}
         ret['ResponseData'] = manage.get_my_app(app_id)
     except CommandException as ce:
         ret = {}
