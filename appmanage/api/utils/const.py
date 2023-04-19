99a5264a10d27d66f16be0acb655681be52fc78d
qiaofeng1227 <76487013@qq.com>
2023-04-19 11:01:02 +0800

    Update const.py

--- appmanage/api/utils/const.py
+++ appmanage/api/utils/const.py
@@ -5,10 +5,18 @@ ERROR_CLIENT_PARAM_BLANK="Client.Parameter.Blank.Error"
 ERROR_CLIENT_PARAM_Format="Client.Parameter.Format.Error"
 ERROR_CLIENT_PARAM_NOTEXIST="Client.Parameter.Value.NotExist.Error"
 ERROR_CLIENT_PARAM_REPEAT="Client.Parameter.Value.Repeat.Error"
-
 ERROR_SERVER_COMMAND="Server.Container.Error"
 ERROR_SERVER_SYSTEM="Server.SystemError"
 ERROR_SERVER_RESOURCE="Server.ResourceError"
+
+# 错误信息定义
+ERRORMESSAGE_CLIENT_PARAM_BLANK="Client.Parameter.Blank.Error"
+ERRORMESSAGE_CLIENT_PARAM_Format="Client.Parameter.Format.Error"
+ERRORMESSAGE_CLIENT_PARAM_NOTEXIST="Client.Parameter.Value.NotExist.Error"
+ERRORMESSAGE_CLIENT_PARAM_REPEAT="Client.Parameter.Value.Repeat.Error"
+ERRORMESSAGE_SERVER_COMMAND="Server.Container.Error"
+ERRORMESSAGE_SERVER_SYSTEM="Server.SystemError"
+ERRORMESSAGE_SERVER_RESOURCE="Server.ResourceError"
 
 # 应用状态定义
 # 应用启动中 installing
