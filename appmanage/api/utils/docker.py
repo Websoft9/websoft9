9709a30ec061b16e0f2372f2f2e0e5628b53451a
qiaofeng1227 <76487013@qq.com>
2023-05-15 11:06:36 +0800

    Update docker.py

--- appmanage/api/utils/docker.py
+++ appmanage/api/utils/docker.py
@@ -62,7 +62,7 @@ def check_appid_exist(app_id):
 def check_appid_include_rq(app_id):
     message = ""
     code = None
-    if app_id == None:
+    if app_id == None or app_id == "undefine":
         code = const.ERROR_CLIENT_PARAM_BLANK
         message = "AppID is null"
     elif re.match('^[a-z0-9]+_[a-z0-9]+$', app_id) == None:
