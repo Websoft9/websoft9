130ec8b8116b4109bdcc01e37264c0cdf8c3e772
qiaofeng1227 <76487013@qq.com>
2023-06-30 09:59:08 +0800

    update plugin-password

--- appmanage/api/service/db.py
+++ appmanage/api/service/db.py
@@ -3,17 +3,24 @@ from pydantic import BaseModel
 from api.model.user import User
 import sqlite3
 
-conn = sqlite3.connect('/usr/src/app/database.sqlite', check_same_thread=False)
-cursor = conn.cursor()
-
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 def AppUpdateUser(user_name, password):
-    sql = "UPDATE user SET password=" + password + " WHERE user_name=" + user_name
-    cursor.execute(sql)
+    conn = sqlite3.connect('database.sqlite')
+    cursor = conn.cursor()
+    cursor.execute("UPDATE user SET password=? WHERE user_name=?", ( password,user_name,))
     conn.commit()
+    conn.close()
 
 def AppSearchUsers(user_type):
-    sql = "SELECT * FROM user WHERE user_type=" + user_type
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    return result
+    conn = sqlite3.connect('/usr/src/app/database.sqlite')
+    conn.row_factory = dict_factory
+    cursor = conn.cursor()
+    cursor.execute("SELECT user_name,password,nick_name FROM user WHERE user_type=?", (user_type,))
+    rows = cursor.fetchone()
+    conn.close()
+    return rows
