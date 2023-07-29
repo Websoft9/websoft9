c70914814395938b736da6520153f04f59078b39
qiaofeng1227 <76487013@qq.com>
2023-07-29 11:24:10 +0800

    cdn

--- appmanage/api/utils/const.py
+++ appmanage/api/utils/const.py
@@ -35,4 +35,4 @@ APP_STATUS_FAILED = "failed"
 
 NGINX_URL="http://websoft9-nginxproxymanager:81"
 #ARTIFACT_URL="https://artifact.azureedge.net/release/websoft9"
-ARTIFACT_URL="hw9artifact.blob.core.windows.net/release/websoft9"
+ARTIFACT_URL="https://w9artifact.blob.core.windows.net/release/websoft9"
