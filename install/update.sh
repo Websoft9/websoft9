a82e5f873998a9140d4542082719927356dbc45f
qiaofeng1227 <76487013@qq.com>
2023-07-31 15:13:55 +0800

    no edit

--- install/update.sh
+++ install/update.sh
@@ -282,11 +282,11 @@ pkcon update -y >/dev/null 2>&1
 # elif  command -v yum > /dev/null;then 
 #   sudo yum update -y cockpit-navigator
 # fi
-echo "Set cockpit port to 9000 ..." 
-sudo sed -i 's/ListenStream=9090/ListenStream=9000/' /lib/systemd/system/cockpit.socket
-# uninstall plugins
-rm -rf /usr/share/cockpit/apps /usr/share/cockpit/selinux /usr/share/cockpit/kdump /usr/share/cockpit/sosreport /usr/share/cockpit/packagekit
-EditMenu
+# echo "Set cockpit port to 9000 ..." 
+# sudo sed -i 's/ListenStream=9090/ListenStream=9000/' /lib/systemd/system/cockpit.socket
+# # uninstall plugins
+# rm -rf /usr/share/cockpit/apps /usr/share/cockpit/selinux /usr/share/cockpit/kdump /usr/share/cockpit/sosreport /usr/share/cockpit/packagekit
+# EditMenu
 
 }
 
