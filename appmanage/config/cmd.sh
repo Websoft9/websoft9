f0aa5ccd0379426d8e5d3f7a582a46b6e9dd869d
qiaofeng1227 <76487013@qq.com>
2023-08-02 16:00:41 +0800

    delete images

--- appmanage/config/cmd.sh
+++ appmanage/config/cmd.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # start by supervisord
-nsenter -m -u -i -n -p -t 1 sh -c  "echo test_host >> /tmp/hosts"
+internal_ip=$(ip addr show eth0 | awk '/inet /{split($2, a, "/"); print a[1]}')
+nsenter -m -u -i -n -p -t 1 sh -c  "echo test_host:$internal_ip >> /tmp/hosts"
 nsenter -m -u -i -n -p -t 1 sh -c  "echo $(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' websoft9-appmanage) websoft9-appmanage >> /etc/hosts"
 /usr/bin/supervisord
 supervisorctl start all
