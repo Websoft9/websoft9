8c6dde9fc3a46743deba95dedc17a77c04c1f1f8
qiaofeng1227 <76487013@qq.com>
2023-08-02 16:53:47 +0800

    hostname improve

--- appmanage/config/cmd.sh
+++ appmanage/config/cmd.sh
@@ -2,8 +2,7 @@
 
 # start by supervisord
 internal_ip=$(ip addr show eth0 | awk '/inet /{split($2, a, "/"); print a[1]}')
-nsenter -m -u -i -n -p -t 1 sh -c  "echo test_host:$internal_ip >> /tmp/hosts"
-nsenter -m -u -i -n -p -t 1 sh -c  "echo $(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' websoft9-appmanage) websoft9-appmanage >> /etc/hosts"
+python3 /usr/src/app/hostname.py
 /usr/bin/supervisord
 supervisorctl start all
 tail -f /dev/null
---
+++ appmanage/hostname.py
@@ -0,0 +1,5 @@
+from api.utils.common_log import myLogger
+from api.utils import shell_execute
+
+shell_execute.execute_command_output_all("sed -i '/websoft9-appmanage/d' /etc/hosts")
+shell_execute.execute_command_output_all("echo $(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' websoft9-appmanage) websoft9-appmanage >> /etc/hosts")
