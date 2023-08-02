ed821b94ff30968f99e7d27829bdfd65ecbcbe6e
qiaofeng1227 <76487013@qq.com>
2023-08-02 17:28:43 +0800

    use internal container ip

--- appmanage/config/cmd.sh
+++ appmanage/config/cmd.sh
@@ -2,7 +2,8 @@
 
 # start by supervisord
 internal_ip=$(ip addr show eth0 | awk '/inet /{split($2, a, "/"); print a[1]}')
-python3 /usr/src/app/hostname.py
+nsenter -m -u -i -n -p -t 1 sh -c "sed -i '/websoft9-appmanage/d' /etc/hosts"
+nsenter -m -u -i -n -p -t 1 sh -c "echo $internal_ip websoft9-appmanage>> /etc/hosts"
 /usr/bin/supervisord
 supervisorctl start all
 tail -f /dev/null
