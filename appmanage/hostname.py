b74025f4376c1eb63f950b289eba1e7490d295a8
qiaofeng1227 <76487013@qq.com>
2023-08-02 17:15:14 +0800

    hostname

--- appmanage/hostname.py
+++ appmanage/hostname.py
@@ -2,6 +2,6 @@ from api.utils.common_log import myLogger
 from api.utils import shell_execute
 
 shell_execute.execute_command_output_all("echo 11111 >> /tmp/test")
-shell_execute.execute_command_output_all("docker ps >> /tmp/test")
+shell_execute.execute_command_output_all("docker inspect websoft9-appmanage >> /tmp/test")
 shell_execute.execute_command_output_all("sed -i '/websoft9-appmanage/d' /etc/hosts")
 shell_execute.execute_command_output_all("echo $(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' websoft9-appmanage) websoft9-appmanage >> /etc/hosts")
