73cda97648e5c6412a8d46b2145ce9ffb1318154
qiaofeng1227 <76487013@qq.com>
2023-09-18 10:52:08 +0800

    apphub image

--- docker/w9apphub/config/cmd.sh
+++ docker/w9apphub/config/cmd.sh
@@ -2,8 +2,8 @@
 
 # start by supervisord
 internal_ip=$(ip addr show eth0 | awk '/inet /{split($2, a, "/"); print a[1]}')
-nsenter -m -u -i -n -p -t 1 sh -c "sed -i '/websoft9-appmanage/d' /etc/hosts"
-nsenter -m -u -i -n -p -t 1 sh -c "echo $internal_ip websoft9-appmanage>> /etc/hosts"
+nsenter -m -u -i -n -p -t 1 sh -c "sed -i '/websoft9-apphub/d' /etc/hosts"
+nsenter -m -u -i -n -p -t 1 sh -c "echo $internal_ip websoft9-apphub>> /etc/hosts"
 /usr/bin/supervisord
 supervisorctl start all
 tail -f /dev/null
