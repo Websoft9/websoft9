0f65442684e0300bfd68613dcc35283ee1ae7efd
qiaofeng1227 <76487013@qq.com>
2023-09-19 18:53:05 +0800

    apphub

--- docker/w9apphub/config/entrypoint.sh
+++ docker/w9apphub/config/entrypoint.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
 # check 
+check_file_exists() {
+    file_path=$1
+    max_attempts=$2
 
+    for ((i=1; i<=max_attempts; i++))
+    do
+        if [ -f "$file_path" ]; then
+            echo "$file_path exists"
+            break
+        else
+            echo "$file_path is not exists, wait a moment.."
+        fi
+        sleep 2
+        if ((i==max_attempts)); then
+            echo "$file_path is not exists, timeout."
+            break
+        fi
+    done
+}
+
+check_file_exists "/websoft9/credentials/proxy" 10
+check_file_exists "/websoft9/credentials/git" 10
+check_file_exists "/websoft9/credentials/deployment" 10
 
 # start by supervisord
-
 /usr/local/bin/supervisord
 supervisorctl start all
 tail -f /dev/null
