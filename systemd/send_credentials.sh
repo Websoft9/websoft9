c99561123e1cee73f5b661f0d9b18d05ae495f95
qiaofeng1227 <76487013@qq.com>
2023-09-20 15:08:31 +0800

    websoft9 servicew

--- systemd/send_credentials.sh
+++ systemd/send_credentials.sh
@@ -6,9 +6,14 @@ trap "sleep 1; continue" ERR
 
 while true; do
     set +e
-    docker cp websoft9-git:/var/websoft9/credential websoft9-apphub:/websoft9/credentials
-    docker cp websoft9-deployment:/var/websoft9/credential websoft9-apphub:/websoft9/credentials
-    docker cp websoft9-proxy:/var/websoft9/credential websoft9-apphub:/websoft9/credentials
+    docker cp websoft9-git:/var/websoft9/credential /data/websoft9/credential_git
+    docker cp /data/websoft9/credential_git websoft9-apphub:/websoft9/credentials
+    
+    docker cp websoft9-deployment:/var/websoft9/credential /data/websoft9/credential_deployment
+    docker cp /data/websoft9/credential_deployment websoft9-apphub:/websoft9/credentials
+    
+    docker cp websoft9-proxy:/var/websoft9/credential /data/websoft9/credential_proxy
+    docker cp /data/websoft9/credential_proxy websoft9-apphub:/websoft9/credentials
     set -e
     sleep 3
 done
