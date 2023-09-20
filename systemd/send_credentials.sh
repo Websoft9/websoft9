ff879c62e9ca24cf1e852ace406e29673fd709a7
qiaofeng1227 <76487013@qq.com>
2023-09-20 17:10:32 +0800

    systemd

--- systemd/send_credentials.sh
+++ systemd/send_credentials.sh
@@ -6,9 +6,12 @@ trap "sleep 1; continue" ERR
 
 while true; do
     set +e
+    
+    echo "copy git credential"
     docker cp websoft9-git:/var/websoft9/credential /data/websoft9/credential_git
     docker cp /data/websoft9/credential_git websoft9-apphub:/websoft9/credentials
     
+    echo "copy deployment credential"
     docker cp websoft9-deployment:/var/websoft9/credential /data/websoft9/credential_deployment
     content=$(cat /data/websoft9/credential_deployment)
     username="admin"
@@ -17,6 +20,7 @@ while true; do
     echo "$json" > /data/websoft9/credential_deployment
     docker cp /data/websoft9/credential_deployment websoft9-apphub:/websoft9/credentials
     
+    echo "wait nginx..."
     docker cp websoft9-proxy:/var/websoft9/credential /data/websoft9/credential_proxy
     docker cp /data/websoft9/credential_proxy websoft9-apphub:/websoft9/credentials
     set -e
