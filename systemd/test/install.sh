2d301c46abb2abeed42d82451b93ce13e9c0aa88
qiaofeng1227 <76487013@qq.com>
2023-09-20 14:59:19 +0800

    test

--- systemd/test/install.sh
+++ systemd/test/install.sh
@@ -5,6 +5,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 export install_path="/data/websoft9"
 
+chmod +x $install_path/systemd/send_credentials.sh
 cp $install_path/systemd/websoft9.service /lib/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable websoft9.service  
