34604224c61108ae56900fa9cb7661f863ddc041
qiaofeng1227 <76487013@qq.com>
2023-09-20 14:56:48 +0800

    systemd

--- systemd/send_credentials.sh
+++ systemd/send_credentials.sh
@@ -5,8 +5,10 @@ set -e
 trap "sleep 1; continue" ERR
 
 while true; do
+    set +e
     docker cp websoft9-git:/var/websoft9/credential websoft9-apphub:/websoft9/credentials
     docker cp websoft9-deployment:/var/websoft9/credential websoft9-apphub:/websoft9/credentials
     docker cp websoft9-proxy:/var/websoft9/credential websoft9-apphub:/websoft9/credentials
+    set -e
     sleep 3
 done
