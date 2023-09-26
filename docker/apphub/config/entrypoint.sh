0c89824b9444eb05d5db27d6335a88bb81905736
qiaofeng1227 <76487013@qq.com>
2023-09-26 17:45:08 +0800

    proxy

--- docker/apphub/config/entrypoint.sh
+++ docker/apphub/config/entrypoint.sh
@@ -40,8 +40,8 @@ git config --global user.email "$password"
 set -e
 
 internal_ip=$(ip addr show eth0 | awk '/inet /{split($2, a, "/"); print a[1]}')
-nsenter -m -u -i -n -p -t 1 sh -c "sed -i '/websoft9-apphub/d' /etc/hosts"
-nsenter -m -u -i -n -p -t 1 sh -c "echo $internal_ip websoft9-apphub>> /etc/hosts"
+sudo nsenter -m -u -i -n -p -t 1 sh -c "sed -i '/websoft9-apphub/d' /etc/hosts"
+sudo nsenter -m -u -i -n -p -t 1 sh -c "echo $internal_ip websoft9-apphub>> /etc/hosts"
 
 # start by supervisord
 /usr/bin/supervisord
