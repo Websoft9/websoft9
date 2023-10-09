91d3a13b53ef12b0e63c603e119f1d933c9aea9a
qiaofeng1227 <76487013@qq.com>
2023-10-09 16:56:46 +0800

    crontab

--- systemd/script/crontab.sh
+++ systemd/script/crontab.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 
-# monitor config.ini, make sure port is the same with cockpit.socket
-
-cockpit_port=from config.ini
-echo "Change cockpit default port to $cockpit_port ..." 
-sudo sed -i "s/ListenStream=9090/ListenStream=$cockpit_port/" /lib/systemd/system/cockpit.socket
-
-
+# monitor /lib/systemd/system/cockpit.socket and config.ini, make sure config.ini port is the same with cockpit.socket
+while true; do
+    inotifywait -e modify /lib/systemd/system/cockpit.socket
+    # 当文件修改时，执行你需要的操作
+done
 
