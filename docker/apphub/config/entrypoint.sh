a122babb959c61e8023316bb8b9d3c0623a0690b
qiaofeng1227 <76487013@qq.com>
2023-10-19 11:13:18 +0800

    rc

--- docker/apphub/config/entrypoint.sh
+++ docker/apphub/config/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-try_times=3
+try_times=5
 
 # start by supervisord
 /usr/bin/supervisord
@@ -12,31 +12,30 @@ supervisorctl start apphub
 # set git user and email
 for ((i=0; i<$try_times; i++)); do
     set +e
-    username=$(apphub getconfig --section gitea --key user_name) 
-    email=$(apphub getconfig --section gitea --key email)
+    username=$(apphub getconfig --section gitea --key user_name 2>/dev/null) 
+    email=$(apphub getconfig --section gitea --key email 2>/dev/null)
     set -e
     if [ -n "$username" ] && [ -n "$email" ]; then
         break
     fi
-    echo "Command failed, retrying..."
+    echo "Wait for service running, retrying..."
     sleep 3
 done
 
-echo $username
-echo $email
-
 if [[ -n "$username" ]]; then
+    echo "git config --global user.name $username"
     git config --global user.name "$username"
 else
-    echo "username is null"
+    echo "username is null, git config username failed"
     exit 1
 fi
 
 regex="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
 if [[ $email =~ $regex ]]; then
+    echo "git config --global user.email $email"
     git config --global user.email "$email"
 else
-    echo "Not have correct email"
+    echo "Not have correct email, git config email failed"
     exit 1
 fi
 
