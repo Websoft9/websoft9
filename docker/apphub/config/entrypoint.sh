17a04798897394bddb8f6b34ed41dbcb70f2953f
qiaofeng1227 <76487013@qq.com>
2023-10-20 16:17:10 +0800

    logs

--- docker/apphub/config/entrypoint.sh
+++ docker/apphub/config/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 
 try_times=5
 
-# start by supervisord
-/usr/bin/supervisord
+# start by supervisord on frontground
+/usr/bin/supervisord -n &
 
 # debug
 supervisorctl start apphub
@@ -39,6 +39,3 @@ else
     echo "Not have correct email, git config email failed"
     exit 1
 fi
-
-sleep 3600
-# tail -f /dev/null
