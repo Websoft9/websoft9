a836e5d7fde99c706101b2e77490db9d77ca49ae
qiaofeng1227 <76487013@qq.com>
2023-10-25 17:26:08 +0800

    proxy

--- docker/proxy/s6/w9init/migration.sh
+++ docker/proxy/s6/w9init/migration.sh
@@ -3,14 +3,14 @@
 set +e 
 
 nginx_proxy(){
-    cp /etc/initproxy.conf /data/nginx/proxy_host/
-    # if [ ! -f /data/nginx/proxy_host/initproxy.conf ] || [ $(stat -c %Y /etc/shadow) -ge $(stat -c %Y /data/nginx/proxy_host/initproxy.conf) ]
-    # then
-    #     cp /etc/initproxy.conf /data/nginx/proxy_host/
-    #     echo "Update initproxy.conf to Nginx"
-    # else
-    #     echo "Don't need to update initproxy.conf to Nginx"
-    # fi
+
+    if [ ! -f /data/nginx/proxy_host/initproxy.conf ] || [ $(stat -c %Y /etc/shadow) -ge $(stat -c %Y /data/nginx/proxy_host) ]
+    then
+        cp /etc/initproxy.conf /data/nginx/proxy_host/
+        echo "Update initproxy.conf to Nginx"
+    else
+        echo "Don't need to update initproxy.conf to Nginx"
+    fi
     
 }
 
