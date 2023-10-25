0324633611068defb6a24864339f9abdf362b333
Darren <27513732@qq.com>
2023-10-25 16:04:56 +0800

    fix

--- docker/proxy/s6/w9init/migration.sh
+++ docker/proxy/s6/w9init/migration.sh
@@ -3,13 +3,15 @@
 set +e 
 
 nginx_proxy(){
-    if [ $(stat -c %Y /etc/shadow) -gt $(stat -c %Y /data/nginx/proxy_host/initproxy.conf) ]
+    
+    if [ ! -f /data/nginx/proxy_host/initproxy.conf ] || [ $(stat -c %Y /etc/shadow) -gt $(stat -c %Y /data/nginx/proxy_host/initproxy.conf) ]
     then
         cp /etc/initproxy.conf /data/nginx/proxy_host/
         echo "Update initproxy.conf to Nginx"
     else
         echo "Don't need to update initproxy.conf to Nginx"
     fi
+
 }
 
 nginx_proxy
