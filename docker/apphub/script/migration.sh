411565edd569a72c9a1c20bd87060f0748c24f4d
qiaofeng1227 <76487013@qq.com>
2023-10-27 17:14:27 +0800

    rc

--- docker/apphub/script/migration.sh
+++ docker/apphub/script/migration.sh
@@ -1,24 +1,27 @@
+#!/bin/bash
+
 echo "start to migrate config.ini"
 
 migrate_ini() {
 
 # Define file paths, use template ini and syn exsit items from target ini
-local target_ini="$1"
-local template_ini="$2"
+export target_ini="$1"
+export template_ini="$2"
 
-/usr/local/bin/python3 - <<EOF
+python3 - <<EOF
 import configparser
 import os
 import sys
+
+target_ini = os.environ['target_ini']
+template_ini = os.environ['template_ini']
 
 # Create two config parsers
 target_parser = configparser.ConfigParser()
 template_parser = configparser.ConfigParser()
 
-target_ini = os.environ['TARGET_INI']
-template_ini = os.environ['TEMPLATE_INI']
-
 try:
+  
     target_parser.read(target_ini)
     template_parser.read(template_ini)
 except configparser.MissingSectionHeaderError:
@@ -32,15 +35,14 @@ for section in target_parser.sections():
             if template_parser.has_option(section, key):
                 template_parser.set(section, key, value)
 
+
 with open(target_ini, 'w') as f:
     template_parser.write(f)
 EOF
 }
 
-export TARGET_INI="/websoft9/apphub/src/config/config.ini"
-export TEMPLATE_INI="/websoft9/config/config.ini"
 
-migrate_ini "$TARGET_INI" "$TEMPLATE_INI"
+migrate_ini "/websoft9/apphub/src/config/config.ini" "/websoft9/config/config.ini"
 
 if [ $? -eq 0 ]; then
     echo "Success to update config.ini"
