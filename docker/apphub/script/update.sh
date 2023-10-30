b38f28dc4f7b4817bfc0402d10fc48e9412b9e47
qiaofeng1227 <76487013@qq.com>
2023-10-30 15:27:57 +0800

    call update method

--- docker/apphub/script/update.sh
+++ docker/apphub/script/update.sh
@@ -5,3 +5,5 @@ echo "$(date) - Compare remote version and local version" | tee -a /var/log/supe
 echo "$(date) - Download remote packages and replace local data" | tee -a /var/log/supervisord.log
 bash update_zip.sh --package_name "media.zip" --sync_to "/websoft9/media"
 bash update_zip.sh --package_name "docker-library.zip" --sync_to "/websoft9/library"
+
+echo "$(date) - Success to update library and media."
--- scripts/update_zip.sh
+++ scripts/update_zip.sh
@@ -111,3 +111,5 @@ upgrade_zip() {
 
     return 0
 }
+
+upgrade_zip
