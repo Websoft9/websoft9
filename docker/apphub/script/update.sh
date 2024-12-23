61c80261de86d45d223277b6e38db4f033b84690
qiaofeng1227 <76487013@qq.com>
2024-12-23 15:05:39 +0800

    apphub 0.1.8

--- docker/apphub/script/update.sh
+++ docker/apphub/script/update.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') - INFO - Compare remote version and local version." | tee -a /var/log/supervisord.log
+channel=release
 
+if [ -f /websoft9/version.json ]; then
+    version=$(cat /websoft9/version.json | jq -r .version)
+    if [[ $version == *rc* ]]; then
+        channel=dev
+    fi
+fi
+
+echo "channel is $channel"
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - INFO - Compare remote version and local version." | tee -a /var/log/supervisord.log
 echo "$(date '+%Y-%m-%d %H:%M:%S') - INFO - Download remote packages and replace local data." | tee -a /var/log/supervisord.log
-bash /websoft9/script/update_zip.sh --package_name "media-latest.zip" --sync_to "/websoft9/media"
-bash /websoft9/script/update_zip.sh --package_name "library-latest.zip" --sync_to "/websoft9/library"
+
+bash /websoft9/script/update_zip.sh --channel $channel --package_name "media-latest.zip" --sync_to "/websoft9/media"
+bash /websoft9/script/update_zip.sh --channel $channel --package_name "library-latest.zip" --sync_to "/websoft9/library"
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - INFO - Success to update library and media."
