bbe14ea51b0302f3559eb6fb3680d9167d047285
qiaofeng1227 <76487013@qq.com>
2025-01-04 11:34:48 +0800

    websoft9

--- install/install.sh
+++ install/install.sh
@@ -200,7 +200,7 @@ export source_unzip="websoft9"
 export source_github_pages="https://websoft9.github.io/websoft9"
 # inotify-tools is at epel-release
 export repo_tools_yum="epel-release"
-export tools_yum="git curl wget yum-utils jq bc unzip inotify-tools"
+export tools_yum="git curl wget jq bc unzip inotify-tools yum-utils"
 export tools_apt="git curl wget jq bc unzip inotify-tools"
 export docker_network="websoft9"
 export artifact_url="https://artifact.websoft9.com/$channel/websoft9"
