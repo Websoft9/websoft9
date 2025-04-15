30e8d6642c27c90e20e55b33a8f2f9ea78ad2109
赵璟 <jing.zhao@websoft9.com>
2025-04-15 10:37:12 +0800

    Update migration.sh

--- docker/apphub/script/migration.sh
+++ docker/apphub/script/migration.sh
@@ -43,8 +43,11 @@ EOF
 # Special migration
 post_migration(){
     echo "$(date '+%Y-%m-%d %H:%M:%S') - INFO - Set listen_port to nginx_proxy_manager"
-    config_file="/websoft9/config/config.ini"
-    listen_port=$(grep -Po '^\s*listen_port\s*=\s*\K[0-9]+' "$config_file")
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - INFO - test"
+    echo $INNER_GATEWAY_PORT
+    #config_file="/websoft9/config/config.ini"
+    #listen_port=$(grep -Po '^\s*listen_port\s*=\s*\K[0-9]+' "$config_file")
+    listen_port=${INNER_GATEWAY_PORT:-80}
     apphub setconfig --section nginx_proxy_manager --key listen_port  --value "$listen_port"
 }
 
